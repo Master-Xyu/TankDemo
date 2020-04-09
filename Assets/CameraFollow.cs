1e13bd185b7a1267aadd5b48bcec42e1f6cf5848
Nicky Huang <hxchlyq@gmail.com>
2020-04-09 20:49:15 +0800

    i.1

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float distance = 15;
+    public float rot = 0;
+    private float roll = 30f * Mathf.PI * 2 / 360;
+    private GameObject target;
+
+    public float rotSpeed = 0.2f;
+
+    private float maxRoll = 70f * Mathf.PI * 2 / 360;
+    private float minRoll = -10f * Mathf.PI * 2 / 360;
+
+    private float rollSpeed = 0.2f;
+
+    public float maxDistance = 22f;
+    public float minDistance = 5f;
+
+    public float zoomSpeed = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetTarget(GameObject.Find("Tank"));
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(target == null)
+        {
+            return;
+        }
+
+        if(Camera.main == null)
+        {
+            return;
+        }
+
+        Vector3 targetPos = target.transform.position;
+
+        Vector3 cameraPos;
+        float d = distance * Mathf.Cos(roll);
+        float height = distance * Mathf.Sin(roll);
+        cameraPos.x = targetPos.x + d * Mathf.Cos(rot);
+        cameraPos.z = targetPos.z + d * Mathf.Sin(rot);
+        cameraPos.y = targetPos.y + height;
+        Camera.main.transform.position = cameraPos;
+        Camera.main.transform.LookAt(target.transform);
+        Rotate();
+        Zoom();
+    }
+
+    void Rotate()
+    {
+        float w = Input.GetAxis("Mouse X") * rotSpeed;
+        rot -= w;
+
+        w = Input.GetAxis("Mouse Y") * rollSpeed * 0.5f;
+        roll -= w;
+
+        if (roll > maxRoll)
+            roll = maxRoll;
+        if (roll < minRoll)
+            roll = minRoll;
+    }
+
+    void Zoom()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if(distance > minDistance)
+            {
+                distance -= zoomSpeed;
+            }
+        }
+        else if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if(distance < maxDistance)
+            {
+                distance += zoomSpeed;
+            }
+        }
+    }
+
+    void SetTarget(GameObject target)
+    {
+        if (target.transform.Find("cameraPoint") != null)
+            this.target = target.transform.Find("cameraPoint").gameObject;
+        else
+            this.target = target;
+    }
+}
---
+++ Assets/Tank.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+
+    public Transform turret;
+    private float turretRotSpeed = 0.5f;
+    private float turretRotTarget = 0;
+
+    public Transform gun;
+    public Transform wheels;
+    public Transform tracks;
+    private float maxRoll = 10f;
+    private float minRoll = -10f;
+    private float turretRollTarget = 0;
+
+    public List<AxleInfo> axleInfos;
+    private float motor = 0;
+    public float maxMotorTorque;
+
+    private float brakeTorque = 0;
+    public float maxBrakeTorque;
+
+    private float steering = 0;
+    public float maxSteeringAngle = 5f;
+
+    public AudioSource motorAudioSource;
+    public AudioClip motorClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        turret = transform.Find("turret");
+        gun = turret.Find("cannon");
+        wheels = transform.Find("wheels");
+        tracks = transform.Find("tracks");
+        motorAudioSource = gameObject.AddComponent<AudioSource>();
+        motorAudioSource.spatialBlend = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerCtrl();
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            axleInfo.leftWheel.brakeTorque = brakeTorque;
+            axleInfo.rightWheel.brakeTorque = brakeTorque;
+
+            if(axleInfos[1] != null && axleInfo == axleInfos[1])
+            {
+                WheelsRotation(axleInfos[1].leftWheel);
+                TrackMove();
+            }
+        }
+
+        TurretRotation();
+        TurretRoll();
+        MotorSound();
+    }
+
+    public void TurretRotation()
+    {
+        if (Camera.main == null)
+            return;
+        if (turret == null)
+            return;
+
+        float angle = turret.eulerAngles.y + 180 - turretRotTarget;
+        if (angle < 0)
+            angle += 360;
+
+        angle %= 360;
+        if (angle > turretRotSpeed && angle < 180)
+            turret.Rotate(0f, -turretRotSpeed, 0f);
+        else if (angle > 180 && angle < 360 - turretRotSpeed)
+            turret.Rotate(0f, turretRotSpeed, 0f);
+    }
+
+    public void TurretRoll()
+    {
+        if (Camera.main == null)
+            return;
+        if (turret == null)
+            return;
+
+        Vector3 worldEuler = gun.eulerAngles;
+        Vector3 localEuler = gun.localEulerAngles;
+
+        worldEuler.x = turretRollTarget;
+        gun.eulerAngles = worldEuler;
+
+        Vector3 euler = gun.localEulerAngles;
+        if (euler.x > 180)
+            euler.x -= 360;
+
+        if (euler.x > maxRoll)
+            euler.x = maxRoll;
+        if (euler.x < minRoll)
+            euler.x = minRoll;
+        gun.localEulerAngles = new Vector3(euler.x, localEuler.y, localEuler.z);
+    }
+
+    public void PlayerCtrl()
+    {
+        maxMotorTorque = 300f;
+        maxSteeringAngle = 25f;
+        motor = -maxMotorTorque * Input.GetAxis("Vertical");
+        steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        turretRollTarget = Camera.main.transform.eulerAngles.x;
+        turretRotTarget = Camera.main.transform.eulerAngles.y;
+
+        brakeTorque = 0;
+        maxBrakeTorque = 400f;
+
+        foreach(AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.leftWheel.rpm > 5 && motor < 0)
+                brakeTorque = maxBrakeTorque;
+            else if (axleInfo.leftWheel.rpm < -5 && motor > 0)
+                brakeTorque = maxBrakeTorque;
+            continue;
+        }
+    }
+
+    public void WheelsRotation(WheelCollider collider)
+    {
+        if (wheels == null)
+            return;
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        foreach(Transform wheel in wheels)
+        {
+            wheel.rotation = rotation;
+        }
+    }
+
+    public void TrackMove()
+    {
+        if (tracks == null)
+            return;
+
+        float offset = 0;
+        if (wheels.GetChild(0) != null)
+            offset = wheels.GetChild(0).localEulerAngles.x / 90f;
+        foreach(Transform track in tracks)
+        {
+            MeshRenderer mr = track.gameObject.GetComponent<MeshRenderer>();
+            if (mr == null)
+                continue;
+            Material mtl = mr.material;
+            mtl.mainTextureOffset = new Vector2(offset, 0);
+        }
+    }
+
+    void MotorSound()
+    {
+        print(motor);
+        if ((motor>1 || motor < -1) && !motorAudioSource.isPlaying)
+        {
+            motorAudioSource.loop = true;
+            motorAudioSource.clip = motorClip;
+            motorAudioSource.Play();
+        }
+        else if(motor < 1 && motor > -1)
+        {
+            motorAudioSource.Pause();
+        }
+    }
+}
